f4925c33520bf7ee5f902f449994a32922fe537c
Mahdi Buchori <mahdibuchori@gmail.com>
2024-01-04 20:44:56 +0700

    up pengadaan

--- src/store/DataPengadaan.js
+++ src/store/DataPengadaan.js
@@ -1,5 +1,5 @@
 import produce from 'immer';
-import create from 'zustand';
+import {create} from 'zustand';
 import { persist } from 'zustand/middleware';
 import { API_AUTH } from '../apis/apisData';
 
